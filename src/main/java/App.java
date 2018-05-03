e39b0dc77cfbddf58ded26d5fb6738608be12bc4
Ruben1996 <rubencobe96@gmail.com>
2018-05-03 18:38:33 +0200

    Retoques app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,47 +3,95 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Retrofit;
+import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.POST;
+import retrofit2.http.Body;
 
 public class App
 {
-    public static final String API_URL = "http://localhost:8080/myapp/";
+    //public static final String API_URL = "https://api.github.com";
+    public static final String API_URL = "http://localhost:8080/myapp/json";
 
-    public static class Jugadores {
-        public final String nombre;
-        public final int vida;
+    public static class Usuario{
 
-        public Jugadores(String nombre, int vida) {
-            this.nombre = nombre;
-            this.vida = vida;
+        public final String user;
+
+        public final String pass;
+
+        public Usuario(String username, String password) {
+            this.user = username;
+
+            this.pass = password;
+        }
+
+    }
+
+    public static class Objeto{}
+
+    public static class Login{
+        public final String username;
+
+        public final String password;
+
+
+        public Login(String username, String password) {
+            this.username = username;
+
+            this.password = password;
         }
     }
 
-    public interface Juego {
-        @GET("/json/users")
-        Call<List<Jugadores>> jugadoress();
+
+    public interface ServiciosRetrofit {
+        @GET("/user/{name}")
+        Call<Usuario> consultarUsuario(@Path("name") String nombre);
+
+        @GET("/obj/{user}/{obj}")
+        Call<Objeto> consultarObjeto (@Path("user") String user,@Path("obj") String nomObj);
+
+        @POST("/user")
+        Call<Response> crearUser (@Body Usuario usuario);
+
     }
 
     public static void main( String[] args ) throws IOException
     {
 
         // Create a very simple REST adapter which points the GitHub API.
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        //Creamos un usuario
+        //Usuario usuario = new Usuario();
 
         // Create an instance of our GitHub API interface.
-        Juego prueva = retrofit.create(Juego.class);
+        ServiciosRetrofit servicios = retrofit.create(ServiciosRetrofit.class);
 
         // Create a call instance for looking up Retrofit contributors.
-        Call<List<Jugadores>> call = prueva.jugadoress();
+        //Call<List<Contributor>> call = github.contributors("RouteInjector", "route-injector");
+        Call<Usuario> call1 = servicios.consultarUsuario("David");
+        Call<Objeto> call2 = servicios.consultarObjeto("David","espada");
+        //Call<Response> call3 = servicios.crearUser(usuario);
 
+
+
+        System.out.println(call1);
+        System.out.println(call2);
         // Fetch and print a list of the contributors to the library.
-        List<Jugadores> jugador = call.execute().body();
-        for (Jugadores jugadores : jugador) {
-            System.out.println(jugadores.nombre + " (" + jugadores.vida + ") ");
+        //List<Contributor> contributors = call.execute().body();
+        Usuario user = call1.execute().body();
+        Objeto obj = call2.execute().body();
+        //Response resp1 = call3.execute().body();
+
+        //for (Contributor contributor: contributors) {
+        /*
+        for (Follower follower : follow) {
+            System.out.println(follower.login + " " + follower.avatar_url);
+            //System.out.println(contributor.login + " (" + contributor.contributions + ") "+ contributor.avatar_url);
+            //System.out.println(contributor.login + ( + contributor.)
         }
+        */
     }
 }
